6ea654db3824341ae2f4d0633a3cba72cfb5af98
balewpewpew <supapitchaitan.2006@gmail.com>
2026-01-06 19:10:27 +0700

    lab13_1.cpp completed!!

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,21 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double x[],int y,double z[]){
+    double sum = 0, power = 0, multi=1, frac=0, max=x[0], min = x[0];
+    for(int i=0;i<y;i++){
+        sum += x[i];
+        power += pow(x[i],2);
+        multi *= x[i];
+        frac += 1/x[i];
+        if(x[i]>max) max = x[i];
+        if(x[i]<min) min = x[i]; 
+    }
+    z[0] = sum/y;
+    z[1] = sqrt((power/y)-pow(z[0],2));
+    z[2] = pow(multi,pow(y,-1));
+    z[3] = y/frac;
+    z[4] = max;
+    z[5] = min;
+}
