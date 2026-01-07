159d599d9fc2fcdf15727b57329780e8383d54b4
balewpewpew <supapitchaitan.2006@gmail.com>
2026-01-07 16:51:21 +0700

    lab13_2,cpp completed !!

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,33 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+
+
+void showImage(const bool x[][M]){
+    for(int i=0;i<=N+1;i++){
+        for(int j=0;j<=M+1;j++){
+            if(i!=0&&i!=N+1){
+                if(j==0 || j==M+1) cout << "|";
+            }
+            if(i==0 || i==N+1) cout << "-";
+            if(i>0&&i<N+1&&j>0&&j<M+1){
+                if(x[i-1][j-1]) cout << "*";
+                else cout << " ";
+            }
+        }
+        cout << "\n";
+    }
+}
+
+void updateImage(bool im[][M],int size,int x,int y){
+    for(int i=0;i<=N+1;i++){
+        for(int j=0;j<=M+1;j++){
+            if((sqrt(pow((i-x),2)+(pow((j-y),2))))<=size-1){
+                if(i<N&&j<M){
+                    im[i][j] = true;
+                }
+            }
+        }
+    }     
+}
